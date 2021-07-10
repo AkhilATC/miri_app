7d811b08bdd63d9101d68351465721ceb900e08a
AkhilATC <akhil.cheriyan@cognicor.com>
2021-07-10 14:54:11 +0530

    changes

---
+++ miRi/Scripts/xhtml2pdf-script.py
@@ -0,0 +1,12 @@
+#!c:\users\akhil\desktop\miri_app\miri_app\miri\scripts\python3.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'xhtml2pdf==0.2.5','console_scripts','xhtml2pdf'
+__requires__ = 'xhtml2pdf==0.2.5'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('xhtml2pdf==0.2.5', 'console_scripts', 'xhtml2pdf')()
+    )
